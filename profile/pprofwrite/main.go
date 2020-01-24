394980da7ff2099f0d282056ee34d2f76cb5b533
Oren Rosen <oren@minutemedia.com>
2020-01-24 16:54:57 +0200

    stuff

--- profile/pprofwrite/main.go
+++ profile/pprofwrite/main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"os"
-	//"os"
-	//"runtime/pprof"
-	"runtime/pprof"
-	"time"
-)
-
 var s, s1, s2 []byte
 
 func main() {
@@ -15,20 +7,16 @@ func main() {
 }
 
 func leakyFunction() {
-	allocS1()
-	allocS2()
-
 	for i:= 0; i < 10 * 1024 * 1024; i++{
-		if i == 3000 { time.Sleep(500 * time.Millisecond) }
 		s = append(s, 's')
 	}
 
-	_ = pprof.WriteHeapProfile(os.Stdout)
+	allocS1()
+	allocS2()
 }
 
 func allocS1() {
 	for i:= 0; i < 10 * 1024 * 1024; i++{
-		if i == 3000 { time.Sleep(500 * time.Millisecond) }
 		s1 = append(s1, 's')
 	}
 }
@@ -36,7 +24,6 @@ func allocS1() {
 
 func allocS2() {
 	for i:= 0; i < 10 * 1024 * 1024; i++{
-		if i == 3000 { time.Sleep(500 * time.Millisecond) }
 		s2 = append(s2, 's')
 	}
 }
